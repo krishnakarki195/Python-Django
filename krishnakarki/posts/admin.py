7d0867dbe863f70130aa3d6a8184cb9fcd66c9ea
krishnakarki195 <krishna.karki195@gmial.com>
2016-09-20 15:38:05 -0500

    posts app created and basic settings were created

--- krishnakarki/posts/admin.py
+++ krishnakarki/posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+	"""docstring for PostAdmin"""
+	list_display = ["title","updated", "timestamp"]
+	list_filter = ["updated", "timestamp"]
+	list_display_links = ["updated"]
+	search_fields = ["title", "content"]
+	list_editable = ["title"]
+
+	class meta:
+		"""docstring for meta"""
+		model = Post
+
+
+admin.site.register(Post,PostModelAdmin)
